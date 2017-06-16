4abc100eed72c4a121fe545b144737a2f8ab8d0e
SzechuanSage <chris.hadgis@gmail.com>
2017-06-16 23:31:39 +1000

    Added initial files

---
+++ src/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by hadgi on 16/06/2017.
+ */
+
+'use strict';
+
+console.log('app.js is working');
---
+++ test/dummy.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hadgi on 16/06/2017.
+ */
+
+import { expect } from 'chai';
+
+describe('dummy', () => {
+  it('does nothing', () => {
+    expect(1).to.equal(1);
+  });
+});
