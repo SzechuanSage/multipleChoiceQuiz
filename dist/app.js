98bcae35fd13311325c227308f54c76dcc95de0b
SzechuanSage <chris.hadgis@gmail.com>
2017-06-19 22:47:49 +1000

    Added original code from www.sitepoint.com

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by hadgi on 16/06/2017.
- */
-
-'use strict';
-
-console.log('app.js is working');
--- src/app.js
+++ src/app.js
@@ -2,6 +2,85 @@
  * Created by hadgi on 16/06/2017.
  */
 
-'use strict';
+const myQuestions = [
+  {
+    question: 'Who is the strongest?',
+    answers: {
+      a: 'Superman',
+      b: 'The Terminator',
+      c: 'Waluigi, obviously',
+    },
+    correctAnswer: 'c',
+  },
+  {
+    question: 'What is the best site ever created?',
+    answers: {
+      a: 'SitePoint',
+      b: 'Simple Steps Code',
+      c: "Trick question; they're both the best",
+    },
+    correctAnswer: 'c',
+  },
+  {
+    question: 'Where is Waldo really?',
+    answers: {
+      a: 'Antarctica',
+      b: 'Exploring the Pacific Ocean',
+      c: 'Sitting in a tree',
+      d: 'Minding his own business, so stop asking',
+    },
+    correctAnswer: 'd',
+  },
+];
 
-console.log('app.js is working');
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function buildQuiz() {
+  const output = [];
+
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+      const answers = [];
+
+      Object.keys(currentQuestion.answers).forEach((letter) => {
+        answers.push(`<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`);
+      });
+
+      output.push(`<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`);
+    });
+
+  quizContainer.innerHTML = output.join('');
+}
+
+function showResults() {
+  const answerContainers = quizContainer.querySelectorAll('.answers');
+
+  let numCorrect = 0;
+
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    const answerContainer = answerContainers[questionNumber];
+    const selector = ['input[name=question', questionNumber, ']:checked'].join('');
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    if (userAnswer === currentQuestion.correctAnswer) {
+      numCorrect += 1;
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    } else {
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
+
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+buildQuiz();
+
+submitButton.addEventListener('click', showResults);
