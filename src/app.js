72b81a502318549a0180a5a0437b830f829c0227
SzechuanSage <chris.hadgis@gmail.com>
2017-06-21 22:33:52 +1000

    Added pagination from www.sitepoint.com

--- src/app.js
+++ src/app.js
@@ -36,6 +36,10 @@ const myQuestions = [
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
+let slides = null;
+let currentSlide = 0;
 
 function buildQuiz() {
   const output = [];
@@ -52,11 +56,16 @@ function buildQuiz() {
           </label>`);
       });
 
-      output.push(`<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`);
+      output.push(`
+      <div class="slide">
+        <div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>
+      </div>`);
     });
 
   quizContainer.innerHTML = output.join('');
+
+  slides = document.querySelectorAll('.slide');
 }
 
 function showResults() {
@@ -81,6 +90,36 @@ function showResults() {
   resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active-slide');
+  slides[n].classList.add('active-slide');
+  currentSlide = n;
+  if (currentSlide === 0) {
+    previousButton.style.display = 'none';
+  } else {
+    previousButton.style.display = 'inline-block';
+  }
+  if (currentSlide === slides.length - 1) {
+    nextButton.style.display = 'none';
+    submitButton.style.display = 'inline-block';
+  } else {
+    nextButton.style.display = 'inline-block';
+    submitButton.style.display = 'none';
+  }
+}
+
+function showNextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+  showSlide(currentSlide - 1);
+}
+
 buildQuiz();
 
+showSlide(0);
+
 submitButton.addEventListener('click', showResults);
+previousButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
